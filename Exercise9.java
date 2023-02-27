429115558640b76777f76d123695e77548dee557
Teja Vardhan <104910147+Tej0901@users.noreply.github.com>
2023-02-27 12:35:10 +0530

    Add files via upload

---
+++ Exercise9.java
@@ -0,0 +1,32 @@
+package multithreading;
+import java.util.*;
+
+
+class Browser
+{
+	public ArrayList<String>historyArrayList=new ArrayList<String>();
+	public Browser(String HomePage) 
+	{
+		historyArrayList.add(HomePage);
+	}
+	
+	void addHistoryUrl(String url)
+	{
+		
+	}
+	
+	void deleteHistoryUrl()
+	{
+		
+	}
+}
+
+
+public class Exercise9 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
