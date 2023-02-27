099c6621c57ca3b02bf6d3e0eb28e479c2ff3a00
Teja Vardhan <104910147+Tej0901@users.noreply.github.com>
2023-02-27 11:17:56 +0530

    Update Exercise8.java

--- Exercise8.java
+++ Exercise8.java
@@ -6,10 +6,9 @@ import java.util.*;
 class Hacker
 {
 	ArrayList<String>historyReceived=new ArrayList<String>();
-	void getHistoryFromBrowser()
+	void getHistoryFromBrowser(Browser tab)
 	{
-		Browser tabOne = new Browser();
-		historyReceived=tabOne.getHistory();
+		historyReceived=tab.getHistory();
 		writeHistoryToFile(historyReceived);	
 	}
 	
@@ -22,17 +21,13 @@ class Hacker
 	        {
 	            System.out.println("File created: "+ fileObject.getName());
 	        }
-	        else 
-	        {
-	        	File fileObjectOne = new File("HistoryFile.txt");
-	        	fileObjectOne.delete();
-	            File fileObjectTwo=new File("myfile.txt");
-	        }
-	        FileWriter fWriter = new FileWriter("myfile.txt");
+	        FileWriter fWriter = new FileWriter("HistoryFile.txt");
 	        for(String url:urls)
 	        {
 	        	fWriter.write(url);
+	        	fWriter.write("\n");
 	        }
+	        System.out.println("Successfully Written Urls From Browser To History File..");
 	        fWriter.close();
 	    }
 	    catch (IOException e) 
@@ -46,14 +41,47 @@ class Hacker
 		}
 	}
 	
-	void modifyHistoryInFile()
+	void readHistoryFile()
 	{
-		
+		try 
+		{
+		      File fileObject = new File("HistoryFile.txt");
+		      Scanner myReader = new Scanner(fileObject);
+		      System.out.println("Contents In the History File...");
+		      while (myReader.hasNextLine())
+		      {
+		        String data = myReader.nextLine();
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } 
+		catch (FileNotFoundException e) 
+		{
+		      System.out.println("An error occurred! File Not Found!!!");
+		      e.printStackTrace();
+		    }
 	}
 	
-	void writeBackModifiedUrlsToBrowserClass()
+	void modifyHistoryByAddingNewUrlsInFile(String newUrl,Browser tab)
 	{
-		
+		try (FileWriter fWriter = new FileWriter("HistoryFile.txt", true);
+                BufferedWriter bWriter = new BufferedWriter(fWriter);
+                PrintWriter pWriter = new PrintWriter(bWriter);) 
+		{
+            pWriter.println(newUrl);
+            System.out.println(newUrl+" : added successfully to file");
+            writeBackModifiedUrlsToBrowserClass(newUrl,tab);
+        } 
+		catch (IOException e) 
+		{
+			System.out.println("An Error Occurred!!");
+            e.printStackTrace();
+        }
+	}
+	
+	void writeBackModifiedUrlsToBrowserClass(String newUrl,Browser tab)
+	{
+		tab.setHistory(newUrl);
 	}
 }
 
@@ -70,6 +98,15 @@ class Browser
 	{
 		return historyArrayList;
 	}
+	
+	public void printUrls()
+	{
+		for(String url:historyArrayList)
+		{
+			System.out.println(url);
+		}
+		System.out.println();
+	}
 }
 
 
@@ -78,34 +115,45 @@ public class Exercise8
 	public static void main(String[] args)
 	{
 		Hacker hackerOne = new Hacker();
+		Browser tabOne = new Browser();
 		Scanner reader = new Scanner(System.in);
 		while(true)
 		{
-			System.out.println("1. Add url\n2. Get Urls And Write to History File\n3. Read the History File\n4. Modify the History File\n5. Exit");
+			System.out.println("\n1. Add url to browser\n2. Read the Urls from browser\n"
+					+ "3. Get Urls And Write to History File\n4. Read the History File"
+					+ "\n5. Modify the History Filen\n6. Exit");
 			String choice=reader.nextLine();
 			switch (Integer.parseInt(choice)) 
 			{
 				case 1: 
 				{
+					tabOne.setHistory(reader.nextLine());
+					tabOne.printUrls();
 					break;
 				}
 				case 2:
 				{
-					break;
+					tabOne.printUrls();
 				}
 				case 3:
 				{
+					hackerOne.getHistoryFromBrowser(tabOne);
 					break;
 				}
 				case 4:
 				{
+					hackerOne.readHistoryFile();
 					break;
 				}
 				case 5:
 				{
+					hackerOne.modifyHistoryByAddingNewUrlsInFile(reader.nextLine(),tabOne);
+					break;
+				}
+				case 6:
+				{
 					System.out.println("Exiting from program  !!!!");
 					System.exit(0);
-					
 					break;
 				}
 			}
