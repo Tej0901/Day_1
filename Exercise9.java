bc80efe7ae10a4da6fab247680e0076bb377a0df
Teja Vardhan <104910147+Tej0901@users.noreply.github.com>
2023-02-27 15:49:29 +0530

    Update Exercise9.java

--- Exercise9.java
+++ Exercise9.java
@@ -1,12 +1,15 @@
 package multithreading;
 import java.util.*;
 
-public class Exercise9  
+public class Exercise9
 {
 	public static void main(String[] args)
 	{
 		ArrayList<String> historyArrayList=new ArrayList<String>();
-
+//		historyArrayList.add("www.zoho.com");
+//		historyArrayList.add("www.Google.in");
+//		historyArrayList.add("www.pec.edu");
+	
 		// Thread created for read operation
 		Thread readHistory = new Thread(new Runnable() 
 		{
@@ -17,11 +20,12 @@ public class Exercise9
 					Scanner sc = new Scanner(System.in);
 					System.out.println("Enter the element : ");
 					historyArrayList.add(sc.nextLine());
+					System.out.println(historyArrayList);
 					System.out.println("Url Written Successfully");
 				}
 			}
 		});
-
+	
 		//Thread Created for Deletion operation
 		Thread deleteHistory = new Thread(new Runnable() 
 		{
@@ -29,17 +33,29 @@ public class Exercise9
 			{
 				synchronized (historyArrayList)
 				{
-					historyArrayList.remove(historyArrayList.size()-1);
-					System.out.println("Last Url Deleted successfully");
+					try 
+					{
+						historyArrayList.remove(historyArrayList.size()-1);
+						System.out.println("\nLast Url Deleted successfully");
+						System.out.println(historyArrayList);
+					} 
+					catch (Exception e) 
+					{
+						System.out.println("Invalid Deletion "+e);
+					}
 				}
 			}
 		});
-
+		
+		deleteHistory.setPriority(Thread.MAX_PRIORITY);
+		readHistory.setPriority(Thread.MIN_PRIORITY);
 		// Read thread is started
 		readHistory.start();
-
+				
 		// Deletion thread is started
 		deleteHistory.start();
 	}
-
+	
 }
+
+
